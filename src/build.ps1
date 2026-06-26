92219c89e0a476a27cf72c877322f2d5265bfd3e
Bastian Eicher <bastian@eicher.net>
2026-06-26 12:19:22 +0200

    Tweaked build script

--- src/build.ps1
+++ src/build.ps1
@@ -18,7 +18,7 @@ function Run-MSBuild {
 
 # Build
 if ($env:CI) { $ci = "/p:ContinuousIntegrationBuild=True" }
-Run-MSBuild /v:Quiet /t:Restore /t:Build $ci /p:Configuration=Release /p:Version=$Version ZeroInstall.Bootstrap.Builder.slnx
+Run-MSBuild /v:Quiet /Restore /t:Build $ci /p:Configuration=Release /p:Version=$Version ZeroInstall.Bootstrap.Builder.slnx
 Remove-Item ..\artifacts\Release\net48 -Include *.xml,*.pdb
 
 popd
